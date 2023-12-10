06d1c33cb8f81f1b200a15453dfee6ce4423a439
Vasil Koltovich <vasiliy.koltovich@gmail.com>
2023-12-10 14:33:27 +0300

    Update connection.js

--- api/src/connection.js
+++ api/src/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
-
 const connection = "mongodb://54.93.253.87:27017/mongo-test";
-
 const connectDb = () => {
   return mongoose.connect(connection);
 };
-
 module.exports = connectDb;
